456302a1b4afe9a5e5ed75debecc442bff0e9618
xodlf100 <xodlf100@gmail.com>
2024-10-26 22:29:37 +0900

    feat OrderSearch

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.repository;
 
 import jakarta.persistence.EntityManager;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,5 +24,13 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch)
+    public List<Order> findAll(OrderSearch orderSearch) {
+        return em.createQuery("select o from Order o join o.member m" +
+                " where o.status = :status " +
+                " and m.name like :name", Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+                .setMaxResults(1000)
+                .getResultList();
+    }
 }
