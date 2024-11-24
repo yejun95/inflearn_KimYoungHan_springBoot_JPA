9f16d44607de15dabbd697d2739fa00601221e86
xodlf100 <xodlf100@gmail.com>
2024-11-24 23:01:49 +0900

    feat X to many를 dto로 조회

--- jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,8 +1,10 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @Slf4j
 @SpringBootApplication
@@ -10,6 +12,11 @@ public class JpashopApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JpashopApplication.class, args);
+    }
+
+    @Bean
+    Hibernate5JakartaModule hibernate5Module() {
+        return new Hibernate5JakartaModule();
     }
 
 }
---
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,84 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    // 연관 엔티티들이 서로 호출하게 되면서 무한루프에 빠짐
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order d : all) {
+            d.getMember().getName();
+            d.getDelivery().getAddress();
+            List<OrderItem> orderItems = d.getOrderItems();
+            orderItems.stream().forEach(o -> o.getItem().getName());
+        }
+        return all;
+    }
+
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @Data
+    static class OrderDto {
+
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems; // 한번더 dto로 데이터를 받아옴
+
+        public OrderDto(Order o) {
+            orderId = o.getId();
+            name = o.getMember().getName();
+            orderDate = o.getOrderDate();
+            orderStatus = o.getStatus();
+            address = o.getDelivery().getAddress();
+            orderItems = o.getOrderItems().stream()
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .collect(Collectors.toList()); // 엔티티가 나올 때 마다 dto로 변환
+        }
+    }
+
+    @Getter
+    static class OrderItemDto {
+
+        private String itemName;
+        private int orderPrice;
+        private int count;
+
+        public OrderItemDto(OrderItem o) {
+            itemName = o.getItem().getName();
+            orderPrice = o.getOrderPrice();
+            count = o.getOrderPrice();
+        }
+    }
+}
